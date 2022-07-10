d4ea7a750d20094eb7d0f012e882d72dd388ac2a
HOME-DESKTOP <baloonflower554@gmail.com>
2022-07-10 12:22:16 +0900

    capture gesture

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
@@ -63,29 +63,91 @@ const extractData = (touches) =>
             `[${idx}] : ${identifier} | ${clientX} ${clientY} | ${screenX} ${screenY} | ${pageX} ${pageY} | ${radiusX} ${radiusY} | ${force} | ${rotationAngle} | `
     );
 
+const GESTURE = {
+    IDLE: 'Idle',
+    TAP: 'Tap',
+    SWIPE: 'Swipe',
+    PINCH: 'Pinch',
+    ZOOM: 'Zoom',
+    ROTATE: 'Rotate',
+};
+
 function App() {
-    const [start, setStart] = useState([]);
-    const [end, setEnd] = useState([]);
-    const [cancel, setCancel] = useState([]);
-    const [move, setMove] = useState([]);
+    const [touchesRecord, setTouchesRecord] = useState({
+        start: [],
+        move: [],
+        end: [],
+        cancel: [],
+    });
+
+    // 한손으로 끌다가 다른 손가락이 올라와도 핀치, 줌, 로테이션 가능
+    const [gesture, setGesture] = useState(GESTURE.IDLE);
+    const [oldTouches, setOldTouches] = useState([]);
+    const [touches, setTouches] = useState([]);
+
+    useEffect(() => {
+        if (!touches.length) {
+            setGesture(GESTURE.IDLE);
+            setOldTouches([]);
+            return;
+        }
+        if (touches.length === 1) {
+            if (!oldTouches.length) setGesture(GESTURE.TAP);
+            else setGesture(GESTURE.SWIPE);
+        }
+        if (touches.length > 1) {
+        }
+    }, [touches]);
 
     const touchstart = (e) => {
         console.log('touchstart', e);
-        setStart(extractData([...e.touches]));
+        setTouchesRecord({
+            ...touchesRecord,
+            start: extractData([...e.touches]),
+        });
+
+        setOldTouches(touches);
+        setTouches([...e.touches]);
+    };
+    const touchmove = (e) => {
+        console.log('touchmove', e);
+        setTouchesRecord({
+            ...touchesRecord,
+            move: extractData([...e.touches]),
+        });
+
+        setOldTouches(touches);
+        setTouches([...e.touches]);
     };
     const touchend = (e) => {
         e.preventDefault();
         console.log('touchend', e);
-        setEnd(extractData([...e.changedTouches]));
+        setTouchesRecord({
+            ...touchesRecord,
+            end: extractData([...e.changedTouches]),
+        });
+
+        setOldTouches(touches);
+        setTouches(
+            touches.filter(({ identifier }) =>
+                [...e.changedTouches].every((e) => e.identifier !== identifier)
+            )
+        );
     };
     const touchcancel = (e) => {
         e.preventDefault();
         console.log('touchcancel', e);
-        setCancel(extractData([...e.touches]));
-    };
-    const touchmove = (e) => {
-        console.log('touchmove', e);
-        setMove(extractData([...e.touches]));
+        setTouchesRecord({
+            ...touchesRecord,
+            cancel: extractData([...e.changedTouches]),
+        });
+
+        setOldTouches(touches);
+        setTouches(
+            touches.filter(({ identifier }) =>
+                [...e.changedTouches].every((e) => e.identifier !== identifier)
+            )
+        );
     };
 
     return (
@@ -96,16 +158,17 @@ function App() {
             onTouchMove={touchmove}
         >
             <Block>
+                <Head>Gesture: {gesture}</Head>
                 <Text>
                     {
                         'idx | identifier | clientX | clientY | screenX | screenY | pageX | pageY | radiusX | radiusY | force | rotationAngle'
                     }
                 </Text>
             </Block>
-            <PrintBlock title='Touch Start' list={start} />
-            <PrintBlock title='Touch Move' list={move} />
-            <PrintBlock title='Touch End' list={end} />
-            <PrintBlock title='Touch Cancel' list={cancel} />
+            <PrintBlock title='Touch Start' list={touchesRecord.start} />
+            <PrintBlock title='Touch Move' list={touchesRecord.move} />
+            <PrintBlock title='Touch End' list={touchesRecord.end} />
+            <PrintBlock title='Touch Cancel' list={touchesRecord.cancel} />
         </Container>
     );
 }
